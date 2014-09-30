a73001889c2f6d383d2670367325d90aae7c1450
Anthony Dagati <tdagati.cs@gmail.com>
2014-09-29 20:14:25 -0700

    Initial Commit

---
+++ NewBlocstagram/BLCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BLCAppDelegate.h
+//  NewBlocstagram
+//
+//  Created by Anthony Dagati on 9/29/14.
+//  Copyright (c) 2014 Black Rail Capital. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BLCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
