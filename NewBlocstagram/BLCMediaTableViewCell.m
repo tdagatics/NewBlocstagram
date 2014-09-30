62c4695832a6c8dea048b7ac4a5b356c59dc51a1
Anthony Dagati <tdagati.cs@gmail.com>
2014-09-29 23:16:17 -0700

    Update Complex Data Cell checkpoint branch for NewBlocstagram

---
+++ NewBlocstagram/BLCMediaTableViewCell.m
@@ -0,0 +1,156 @@
+//
+//  BLCMediaTableViewCell.m
+//  NewBlocstagram
+//
+//  Created by Anthony Dagati on 9/29/14.
+//  Copyright (c) 2014 Black Rail Capital. All rights reserved.
+//
+
+#import "BLCMediaTableViewCell.h"
+#import "BLCUser.h"
+#import "BLCComment.h"
+#import "BLCMedia.h"
+
+static UIFont *lightFont;
+static UIFont *boldFont;
+static UIColor *usernameLabelGray;
+static UIColor *commentLabelGray;
+static UIColor *linkColor;
+static NSParagraphStyle *paragraphStyle;
+
+@implementation BLCMediaTableViewCell
+
++(void) load {
+    lightFont = [UIFont fontWithName:@"HelveticaNeue-Thin" size:11];
+    boldFont = [UIFont fontWithName:@"HelveticaNeue-Bold" size:11];
+    usernameLabelGray = [UIColor colorWithRed:0.933 green:0.933 blue:0.933 alpha:1]; /*#eeeeee*/
+    commentLabelGray = [UIColor colorWithRed:0.898 green:0.898 blue:0.898 alpha:1]; /*#e5e5e5*/
+    linkColor = [UIColor colorWithRed:0.345 green:0.314 blue:0.427 alpha:1]; /*#58506d*/
+    
+    NSMutableParagraphStyle *mutableParagraphStyle = [[NSMutableParagraphStyle alloc] init];
+    mutableParagraphStyle.headIndent = 20.0;
+    mutableParagraphStyle.firstLineHeadIndent = 20.0;
+    mutableParagraphStyle.tailIndent = -20.0;
+    mutableParagraphStyle.paragraphSpacingBefore = 5;
+    
+    paragraphStyle = mutableParagraphStyle;
+    
+}
+-(id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+    // Initialization code
+        self.mediaImageView = [[UIImageView alloc] init];
+        self.usernameAndCaptionLabel = [[UILabel alloc] init];
+        self.commentLabel = [[UILabel alloc] init];
+        self.commentLabel.numberOfLines = 0;
+    
+        for (UIView *view in @[self.mediaImageView, self.usernameAndCaptionLabel, self.commentLabel]) {
+        [self.contentView addSubview:view];
+        }
+    }
+return self;
+}
+
+-(NSAttributedString *)userNameAndCaptionString {
+    CGFloat usernameFontSize = 15;
+    
+    // Make a string that says "username caption text"
+    NSString *baseString = [NSString stringWithFormat:@"%@ %@", self.mediaItem.user.userName, self.mediaItem.caption];
+    
+    // Make an attributed string, with the "username" bold
+    NSMutableAttributedString *mutableUserNameAndCaptionString = [[NSMutableAttributedString alloc] initWithString:baseString attributes:@{NSFontAttributeName : [lightFont fontWithSize:usernameFontSize], NSParagraphStyleAttributeName : paragraphStyle}];
+    
+    NSRange usernameRange = [baseString rangeOfString:self.mediaItem.user.userName];
+    [mutableUserNameAndCaptionString addAttribute:NSFontAttributeName value:[boldFont fontWithSize:usernameFontSize] range:usernameRange];
+    [mutableUserNameAndCaptionString addAttribute:NSForegroundColorAttributeName value:linkColor range:usernameRange];
+    
+    return mutableUserNameAndCaptionString;
+}
+
+-(NSAttributedString *) commentString {
+    NSMutableAttributedString *commentString = [[NSMutableAttributedString alloc] init];
+    
+    for (BLCComment *comment in self.mediaItem.comments) {
+        // Make a string that says "username comment text" followed by a line break
+        NSString *baseString = [NSString stringWithFormat:@"%@ %@\n", comment.from.userName, comment.text];
+        
+        // Make an attributed string, with the "username" bold
+        
+        NSMutableAttributedString *oneCommentString = [[NSMutableAttributedString alloc] initWithString:baseString attributes:@{NSFontAttributeName : lightFont, NSParagraphStyleAttributeName: paragraphStyle}];
+        
+        NSRange usernameRange = [baseString rangeOfString:comment.from.userName];
+        
+        [oneCommentString addAttribute:NSFontAttributeName value:linkColor range:usernameRange];
+        [oneCommentString addAttribute:NSForegroundColorAttributeName value:linkColor range:usernameRange];
+        [commentString appendAttributedString:oneCommentString];
+    }
+    return commentString;
+}
+
+-(CGSize) sizeOfString:(NSAttributedString *)string {
+    CGSize maxSize = CGSizeMake(CGRectGetWidth(self.contentView.bounds) - 40, 0.0);
+    CGRect sizeRect = [string boundingRectWithSize:maxSize options:NSStringDrawingUsesLineFragmentOrigin context:nil];
+    sizeRect.size.height += 20;
+    sizeRect = CGRectIntegral(sizeRect);
+    return sizeRect.size;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+@end
