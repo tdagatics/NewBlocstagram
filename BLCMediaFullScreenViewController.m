95962f7c8616865ea8502b4e519d70c7f742dbb6
Anthony Dagati <tdagati.cs@gmail.com>
2014-10-15 16:17:51 -0700

    Updated commit of full screen photo viewing

--- BLCMediaFullScreenViewController.m
+++ BLCMediaFullScreenViewController.m
@@ -12,6 +12,8 @@
 @interface BLCMediaFullScreenViewController () <UIScrollViewDelegate>
 
 @property (nonatomic, strong) BLCMedia *media;
+@property (nonatomic, strong) UITapGestureRecognizer *tap;
+@property (nonatomic, strong) UITapGestureRecognizer *doubleTap;
 
 @end
 
@@ -42,7 +44,18 @@
     
     [self.scrollView addSubview:self.imageView];
     self.scrollView.contentSize = self.media.image.size;
+    
+    self.tap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(tapFired:)];
+    
+    self.doubleTap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(doubleTapFired:)];
+    self.doubleTap.numberOfTapsRequired = 2;
+    
+    [self.tap requireGestureRecognizerToFail:self.doubleTap];
+    
+    [self.scrollView addGestureRecognizer:self.tap];
+    [self.scrollView addGestureRecognizer:self.doubleTap];
 }
+
 
 -(void)viewWillLayoutSubviews {
     [super viewWillLayoutSubviews];
@@ -101,6 +114,28 @@
     // Dispose of any resources that can be recreated.
 }
 
+#pragma mark - Gesture Recognizers
+
+-(void)tapFired:(UITapGestureRecognizer *)sender {
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+-(void)doubleTapFired:(UITapGestureRecognizer *)sender {
+    if (self.scrollView.zoomScale == self.scrollView.minimumZoomScale) {
+        CGPoint locationPoint = [sender locationInView:self.imageView];
+        
+        CGSize scrollViewSize = self.scrollView.bounds.size;
+        
+        CGFloat width = scrollViewSize.width / self.scrollView.maximumZoomScale;
+        CGFloat height = scrollViewSize.height / self.scrollView.maximumZoomScale;
+        CGFloat x = locationPoint.x - (width / 2);
+        CGFloat y = locationPoint.y - (height / 2);
+        
+        [self.scrollView zoomToRect:CGRectMake(x, y, width, height) animated:YES];
+    } else {
+        [self.scrollView setZoomScale:self.scrollView.minimumZoomScale animated:YES];
+    }
+}
 /*
 #pragma mark - Navigation
 
@@ -110,5 +145,7 @@
     // Pass the selected object to the new view controller.
 }
 */
+
+
 
 @end
