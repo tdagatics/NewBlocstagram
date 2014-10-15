4a2660f590bf38def8abeee357d9e5c1f7061415
Anthony Dagati <tdagati.cs@gmail.com>
2014-10-15 16:03:09 -0700

    Updated commit for FullScreenViewController

--- BLCMediaFullScreenViewController.m
+++ BLCMediaFullScreenViewController.m
@@ -7,16 +7,93 @@
 //
 
 #import "BLCMediaFullScreenViewController.h"
+#import "BLCMedia.h"
 
-@interface BLCMediaFullScreenViewController ()
+@interface BLCMediaFullScreenViewController () <UIScrollViewDelegate>
+
+@property (nonatomic, strong) BLCMedia *media;
 
 @end
 
 @implementation BLCMediaFullScreenViewController
 
+-(instancetype) initWithMedia:(BLCMedia *)media {
+    self = [super init];
+    
+    if (self) {
+        self.media = media;
+    }
+    
+    return self;
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    self.scrollView = [UIScrollView new];
+    self.scrollView.delegate = self;
+    self.scrollView.backgroundColor = [UIColor whiteColor];
+    
+    [self.view addSubview:self.scrollView];
+    
+    self.imageView = [UIImageView new];
+    self.imageView.image = self.media.image;
+    
+    [self.scrollView addSubview:self.imageView];
+    self.scrollView.contentSize = self.media.image.size;
+}
+
+-(void)viewWillLayoutSubviews {
+    [super viewWillLayoutSubviews];
+    self.scrollView.frame = self.view.bounds;
+    
+    CGSize scrollViewFrameSize = self.scrollView.frame.size;
+    CGSize scrollViewContentSize = self.scrollView.contentSize;
+    
+    CGFloat scaleWidth = scrollViewFrameSize.width / scrollViewContentSize.width;
+    CGFloat scaleHeight = scrollViewFrameSize.height / scrollViewContentSize.height;
+    CGFloat minScale = MIN(scaleWidth, scaleHeight);
+    
+    self.scrollView.minimumZoomScale = minScale;
+    self.scrollView.maximumZoomScale = 1;
+}
+
+-(void)centerScrollView {
+    [self.imageView sizeToFit];
+    
+    CGSize boundsSize = self.scrollView.bounds.size;
+    CGRect contentsFrame = self.imageView.frame;
+    
+    if (contentsFrame.size.width < boundsSize.width) {
+        contentsFrame.origin.x = (boundsSize.width - CGRectGetWidth(contentsFrame)) / 2;
+    } else {
+        contentsFrame.origin.x = 0;
+    }
+    
+    if (contentsFrame.size.height < boundsSize.height) {
+        contentsFrame.origin.y = (boundsSize.height - CGRectGetHeight(contentsFrame))/2;
+    } else {
+        contentsFrame.origin.y = 0;
+    }
+    
+    self.imageView.frame = contentsFrame;
+}
+
+-(void) viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    
+    [self centerScrollView];
+}
+
+#pragma mark - UIScrollViewDelegate
+
+-(UIView *)viewForZoomingInScrollView:(UIScrollView *)scrollView {
+    return self.imageView;
+}
+
+-(void)scrollViewDidZoom:(UIScrollView *)scrollView {
+    [self centerScrollView];
 }
 
 - (void)didReceiveMemoryWarning {
