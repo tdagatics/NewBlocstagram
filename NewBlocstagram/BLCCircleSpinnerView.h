e746f1fb3288f7e586fd0184267b820608ecc70e
Anthony Dagati <tdagati.cs@gmail.com>
2014-11-04 10:22:49 -0800

    Temporary commit of like button checkpoint

---
+++ NewBlocstagram/BLCCircleSpinnerView.h
@@ -0,0 +1,17 @@
+//
+//  BLCCircleSpinnerView.h
+//  NewBlocstagram
+//
+//  Created by Anthony Dagati on 11/3/14.
+//  Copyright (c) 2014 Black Rail Capital. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BLCCircleSpinnerView : UIView
+
+@property (nonatomic, assign) CGFloat strokeThickness;
+@property (nonatomic, assign) CGFloat radius;
+@property (nonatomic, strong) UIColor *strokeColor;
+
+@end
---
+++ NewBlocstagram/BLCLikeButton.m
@@ -0,0 +1,88 @@
+//
+//  BLCLikeButton.m
+//  NewBlocstagram
+//
+//  Created by Anthony Dagati on 11/4/14.
+//  Copyright (c) 2014 Black Rail Capital. All rights reserved.
+//
+
+#import "BLCLikeButton.h"
+#import "BLCCircleSpinnerView.h"
+
+#define kLikedStateImage @"heart-full"
+#define kUnlikedStateImage @"Heart-empty"
+
+@interface BLCLikeButton ()
+
+@property (nonatomic, strong) BLCCircleSpinnerView *spinnerView;
+
+@end
+
+@implementation BLCLikeButton
+
+-(instancetype) init {
+    self = [super init];
+    
+    if (self) {
+        self.spinnerView = [[BLCCircleSpinnerView alloc] initWithFrame:CGRectMake(0, 0, 44, 44)];
+        [self addSubview:self.spinnerView];
+        
+        self.imageView.contentMode = UIViewContentModeScaleAspectFit;
+        
+        self.contentEdgeInsets = UIEdgeInsetsMake(10, 10, 10, 10);
+        self.contentVerticalAlignment = UIControlContentVerticalAlignmentTop;
+
+        self.likeButtonState = BLCLikeStateNotLiked;
+    }
+    
+    return self;
+}
+
+-(void)layoutSubviews {
+    [super layoutSubviews];
+    self.spinnerView.frame = self.imageView.frame;
+}
+
+-(void)setLikeButtonState:(BLCLikeState)likeState
+{
+    _likeButtonState = likeState;
+    
+    NSString *imageName;
+    
+    switch (_likeButtonState) {
+        case BLCLikeStateLiked:
+        case BLCLikeStateUnliking:
+            imageName = kLikedStateImage;
+            break;
+            
+        case BLCLikeStateNotLiked:
+        case BLCLikeStateLiking:
+            imageName = kLikedStateImage;
+            break;
+    }
+    
+    switch (_likeButtonState) {
+        case BLCLikeStateLiking:
+        case BLCLikeStateUnliking:
+            self.spinnerView.hidden = NO;
+            self.userInteractionEnabled = NO;
+            break;
+            
+        case BLCLikeStateLiked:
+        case BLCLikeStateNotLiked:
+            self.spinnerView.hidden = YES;
+            self.userInteractionEnabled = YES;
+    }
+    
+    [self setImage:[UIImage imageNamed:imageName] forState:UIControlStateNormal];
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
